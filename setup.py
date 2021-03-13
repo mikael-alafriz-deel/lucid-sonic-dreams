b28852cfe508bae65e83ba10811de78ee8cf493d
unknown <mikaelalafriz@gmail.com>
2021-03-13 13:52:56 +0100

    Fixed requirements

--- setup.py
+++ setup.py
@@ -2,24 +2,35 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-with open('requires.txt') as f:
-    required = f.read().splitlines()
 
 setuptools.setup(
     name="lucidsonicdreams", 
-    version="0.3",
+    version="0.4",
     author="Alain Mikael Alafriz",
     author_email="mikaelalafriz@gmail.com",
     description="Syncs GAN-generated visuals to music",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mikaelalafriz/lucid-sonic-dreams",
-    download_url="https://github.com/mikaelalafriz/lucid-sonic-dreams/archive/v_03.tar.gz",
+    download_url="https://github.com/mikaelalafriz/lucid-sonic-dreams/archive/v_04.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=required
+    install_requires=['tensorflow==1.15',
+                      'librosa',
+                      'numpy',
+                      'moviepy',
+                      'Pillow',
+                      'tqdm',
+                      'scipy',
+                      'scikit-image',
+                      'pygit2',
+                      'gdown', 
+                      'mega.py',
+                      'requests',
+                      'pandas',
+                      'SoundFile']
 )
